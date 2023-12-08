343767e9a91b13b107616d92f92f63db9ef58ab5
GitHub Action <action@github.com>
2023-12-08 22:43:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/temofey1989/serverlessworkflowplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/temofey1989/serverlessworkflowplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.temofey1989.serverlessworkflowplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.temofey1989.serverlessworkflowplugin.MyBundle
+import com.github.temofey1989.serverlessworkflowplugin.services.MyProjectService
 import javax.swing.JButton
 
 
